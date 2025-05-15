0929b7d1c35919bd2efed94e893c18cfe8408eed
lilyphae <22012672@hope.ac.uk>
2025-05-15 10:19:36 +0100

    Radio Buttons
    Fixed radio button functionality
    FAB navigates to second fragment to add moods
    Moods basically displayed on first fragment

--- app/src/main/java/uk/ac/hope/mcse/android/coursework/SecondFragment.java
+++ app/src/main/java/uk/ac/hope/mcse/android/coursework/SecondFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,44 +13,35 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import uk.ac.hope.mcse.android.coursework.databinding.FragmentSecondBinding;
 
+
 public class SecondFragment extends Fragment {
 
-    private FragmentSecondBinding binding;
-
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.saveMoodButton.setOnClickListener(v -> {
-            int selectedId = binding.moodRadioGroup.getCheckedRadioButtonId();
-            String mood = "";
-            if (selectedId != -1) {
-                switch (selectedId) {
-                    case R.id.radioHappy:
-                        mood = "Happy";
-                        break;
-                    case R.id.radioSad:
-                        mood = "Sad";
-                        break;
-                    case R.id.radioAngry:
-                        mood = "Angry";
-                        break;
-                    case R.id.radioNeutral:
-                        mood = "neutral";
-                        break;
-                }
+        RadioGroup moodRadioGroup = view.findViewById(R.id.moodRadioGroup);
+        Button saveMoodButton = view.findViewById(R.id.saveMoodButton);
 
+        saveMoodButton.setOnClickListener(v -> {
+            int selectedId = moodRadioGroup.getCheckedRadioButtonId();
+            String mood = "";
+
+            if (selectedId != -1) {
+                if (selectedId == R.id.radioHappy) {
+                    mood = "Happy";
+                } else if (selectedId == R.id.radioSad) {
+                    mood = "Sad";
+                } else if (selectedId == R.id.radioAngry) {
+                    mood = "Angry";
+                } else if (selectedId == R.id.radioNeutral) {
+                    mood = "Neutral";
+                }
 
                 MoodStore.addMood(mood);
 
@@ -61,7 +54,6 @@ public class SecondFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 
 }
